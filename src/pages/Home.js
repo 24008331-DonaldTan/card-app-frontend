c13a703052c5c263d0d3fea5bb84ab919ac1c7f7
24008331-DonaldTan <24008331@myrp.edu.sg>
2026-01-20 10:33:19 +0800

    Fix Home page and remove ESLint error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  /* TODO: Design and complete the Home page
-    - display instructions
-    - link to Cards page
-    - style as a landing page */
-  return <main></main>;
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Card App</h1>
+
+      <p>
+        This application allows you to view, add, and edit cards stored in the
+        database.
+      </p>
+
+      <nav style={{ marginTop: "1.5rem" }}>
+        <Link to="/cards">View Cards</Link>
+        <br />
+        <Link to="/add">Add New Card</Link>
+      </nav>
+    </main>
+  );
 }
